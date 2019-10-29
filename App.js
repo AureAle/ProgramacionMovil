a2bf8c1bae43269c475bb387e9aa351c5117ecd7
7e <7e@iMac-de-iMac21.local>
2019-10-29 09:55:21 -0600

    login

--- App.js
+++ App.js
@@ -7,7 +7,17 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View, Button, Switch,TextInput, Alert } from "react-native";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Switch,
+  Image,
+  TextInput,
+  Alert
+} from "react-native";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -22,43 +32,53 @@ export default class App extends Component<Props> {
     super(props);
     this.state = {
       switchValue: false,
-      text: ''
+      email: "",
+      password: ""
     };
   }
 
-  onChange = (value) => {
-    console.warn(`El switch cambiará a : ${value}`)
-    this.setState({switchValue: value})
-  }
+  onChange = value => {
+    console.warn(`El switch cambiará a : ${value}`);
+    this.setState({ switchValue: value });
+  };
   onPressLearnMore() {
     console.warn("Presionaste el botón");
   }
   changeInput = () => {
-    const { text } = this.state;
-    Alert.alert(text)
-  }
+    const { email } = this.state;
+    const { password } = this.state
+    Alert.alert("E-mail: " + email +" " +"Password: "+ password);
+  };
 
   render() {
     return (
       <View style={styles.container}>
-      <TextInput 
-        placeholder= 'type...'    
-        maxLength={20}
-        onChangeText={text => this.setState({text})}
-        value={this.state.text}
+      <Image 
+          style={{ width: 200, height: 200 }}
+          source={require("./React.png")}
+      />
+      <Text style={styles.text}>Welcome Humans!</Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholderTextColor="white"
+          placeholder="Email"
+          onChangeText={email => this.setState({email})}
+          value={this.state.email}
         />
-      
-        <Switch
-          onValueChange={() => this.onChange(!this.state.switchValue)}
-          value={this.state.switchValue}
+        <TextInput
+          style={styles.inputBox}
+          placeholderTextColor="white"
+          secureTextEntry={true}
+          placeholder="Password"
+          onChangeText={password => this.setState({password})}
+          value={this.state.password}
         />
-        <Button
+        <TouchableOpacity
+          style={styles.button}
           onPress={this.changeInput}
-          title="Learn more"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Text style={styles.welcome}>My Music AureAle!!!!</Text>
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -67,19 +87,39 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    justifyContent: 'center',
+    flexDirection:'column',
+    alignItems:'center',
+    backgroundColor: "black"
   },
-  welcome: {
-    fontSize: 40,
-    textAlign: "center",
-    margin: 10,
-    color: "#000000"
+  button: {
+    width: 100,
+    backgroundColor: "#d8577f",
+    borderRadius: 5,
+    marginVertical: 10,
+    paddingVertical: 10
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "white",
+    textAlign: "center"
+  },
+  inputBox: {
+    width: 300,
+    backgroundColor: "pink",
+    paddingHorizontal: 16,
+    fontSize: 18,
+    color: "#ffff",
+    marginVertical: 10,
+    padding:10
+  },
+  input: {
+    fontSize: 20,
+    margin: 10
+  },
+  text:{
+    fontSize:30,
+    color:'white'
   }
 });
